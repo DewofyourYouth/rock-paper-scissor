68095ad9554948a9cfab8a248f1d0bc3d06f5d63
Jacob Shore <jacobshore@gmail.com>
2020-01-20 08:33:45 +0200

    score

--- js/index.js
+++ js/index.js
@@ -1,14 +1,11 @@
 const { createStore } = Redux;
 
-const counter = (state = 0, action) => {
+const counter = (state = { you: 0, computer: 0 }, action) => {
   switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      if (state > 0) {
-        return state - 1;
-      }
-      return state;
+    case 'WIN':
+      return { you: state.you + 1, computer: state.computer };
+    case 'LOSE':
+      return { you: state.you, computer: state.computer + 1 };
     default:
       return state;
   }
@@ -18,13 +15,20 @@ const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 const results = document.getElementById('results');
-let yourScore = 0;
-let computerScore = 0;
 const rpsArray = [
   { name: 'rock', el: rock },
   { name: 'paper', el: paper },
   { name: 'scissors', el: scissors }
 ];
+
+const displayScore = () => {
+  document.getElementById('urscore').innerText = store.getState().you
+    ? store.getState().you
+    : 0;
+  document.getElementById('compscore').innerText = store.getState().computer
+    ? store.getState().computer
+    : 0;
+};
 
 let store = createStore(counter);
 const checkGame = (you, computer) => {
@@ -35,18 +39,12 @@ const checkGame = (you, computer) => {
     (you === 'scissors' && computer === 'paper') ||
     (you === 'paper' && computer === 'rock')
   ) {
-    store.dispatch({ type: 'INCREMENT' });
-    if (store.getState()) {
-      console.log('your score ' + store.getState());
-    }
-    document.getElementById('urscore').innerText = store.getState()
-      ? store.getState()
-      : 0;
+    store.dispatch({ type: 'WIN' });
+    displayScore();
     return 'You Win!';
   } else {
-    store.dispatch({ type: 'DECREMENT' });
-    console.log('your score' + store.getState());
-    document.getElementById('urscore').innerText = store.getState();
+    store.dispatch({ type: 'LOSE' });
+    displayScore();
     return 'You Lose!';
   }
 };
@@ -57,9 +55,5 @@ rpsArray.forEach(choice => {
     let computer = rpsArray[Math.floor(Math.random() * 3)].name;
     results.innerHTML = `<h2>You: ${choice.name.toUpperCase()}, Computer: ${computer.toUpperCase()}</h2>`;
     results.innerHTML += `<h1>${checkGame(choice.name, computer)}</h1>`;
-    console.table({
-      you: { choice: choice.name },
-      computer: { choice: computer }
-    });
   });
 });
